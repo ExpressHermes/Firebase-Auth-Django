64e7ac3f05fab243ac0fcb572b679d6d13d17ae6
ExpressHermes <9876shivamyadav@gmail.com>
2020-11-25 10:38:46 +0530

    UI changes
    - Added bootstrap files to static folder
    - Added form help in signup page
    - Made some UI changes to alerts and other pages

--- firebase_auth/views.py
+++ firebase_auth/views.py
@@ -11,7 +11,7 @@ db = firebase.database()
 
 # Create your views here.
 def profile(request):
-    pp(dict(request.session))
+    # If the user is not signed in, redirect to signin page
     try:
         user_id = dict(request.session)['token_id']
         response = db.child("users").get(user_id).val()
@@ -24,11 +24,10 @@ def profile(request):
         return redirect('firebase_auth:signin')
     
     if request.method == "POST":
-        form = request.POST
-        place_of_birth = form.get('place_of_birth')
+        place_of_birth = request.POST.get('place_of_birth')
         user['place_of_birth'] = place_of_birth
         try:
-            # Pass the user's idToken to the push method
+            # update user 
             db.child("users").child(user_info[0]).update(user)
         except Exception as e:
             pp(e)
@@ -37,17 +36,15 @@ def profile(request):
 
 def signup(request, *args, **kwargs):
     if request.method == "POST":
-        form = request.POST
-        name = form.get('name')
-        email = form.get('email')
-        password = form.get('password')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
         try:
             user = auth.create_user_with_email_and_password(email, password)
-            # add user to db
+            # add user to database
             data = {
                 "name": name, 
                 "email": email,
-                "place_of_birth": None,
                 "status": 1
             }
             # Pass the user's idToken to the push method
@@ -56,34 +53,30 @@ def signup(request, *args, **kwargs):
             return redirect('firebase_auth:signin')
         except Exception as e:
             pp(e)
-            messages.error(request, 'Signup Failed')
+            messages.error(request, 'Signup Failed! Make sure email is unique and password is 6 characters long')
     return render(request, 'auth/signup.html')
 
 def signin(request, *args, **kwargs):
     if request.method == "POST":
-        form = request.POST
-        email = form.get('email')
-        password = form.get('password')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
         try:
             user = auth.sign_in_with_email_and_password(email, password)
-            pp(dict(request.session))
+            # starting a django session
             request.session['token_id'] = str(user['idToken'])
-            pp(dict(request.session))
             messages.success(request, 'Signed In!')
             return redirect('firebase_auth:profile')
         except Exception as e:
             pp(e)
-            messages.error(request, 'Signin Failed')
+            messages.error(request, 'Signin Failed! Invalid credentials')
     return render(request, 'auth/signin.html')
 
 def logout_view(request):
     try:
-        pp(dict(request.session))
+        # delete session token
         del request.session['token_id']
-        pp(dict(request.session))
         messages.success(request, "Logged out successfully")
     except Exception as e:
-        pp(dict(request.session))
         print(e)
         messages.error(request, "Logout Failed")
         return redirect('firebase_auth:profile')
